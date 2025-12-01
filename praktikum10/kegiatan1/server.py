fb2ad034682beac937e166bcb4f6c6888758cbeb
Zaidan07 <zaidanxgaming@gmail.com>
2025-12-01 09:00:15 +0700

    fix(activity2): shorten lines code

--- praktikum10/kegiatan1/server.py
+++ praktikum10/kegiatan1/server.py
@@ -1,26 +1,24 @@
 import socket
 
-nama = "Muhammad Zaidan Nabih"
-nim = "L200250078"
-angkatan = "2025"
+data = {
+    "nama": "Muhammad Zaidan Nabih",
+    "nim": "L200250078",
+    "angkatan": "2025"
+}
 
 s = socket.socket()
 s.bind(("localhost", 5000))
 s.listen(1)
 
 print("Server siap...")
-c, a = s.accept()
+c, a = s.accept()   
 print("Terhubung dengan : ", a)
 
 while True :
     perintah = c.recv(1024).decode().lower()
     
-    if perintah == "nama":
-        c.send(nama.encode())
-    elif perintah == "nim":
-        c.send(nim.encode())
-    elif perintah == "angkatan":
-        c.send(angkatan.encode())
+    if perintah in data:
+        c.send(data[perintah].encode())
     elif perintah == "keluar":
         c.send("siap!".encode())
         break
